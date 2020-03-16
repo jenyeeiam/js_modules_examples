ca5d9962649de39e01f1e282c96811d8fa252436
Jen Yee <jenyee@Jens-MacBook-Pro.local>
2020-03-15 20:14:39 -0700

    3 tests for dog

--- test/test.dog.js
+++ test/test.dog.js
@@ -1,10 +1,22 @@
 var assert = require('assert');
 
-var dog = require('../src/dog.js')
+// var dog = require('../src/dog.js');
+import { Doggio } from '../src/dog.js';
+const sparky = new Doggio(10, 30, 'Sparky');
 
 describe('Dog class', function() {
-  it('should do something', function() {
-    console.log(dog)
-    assert.equal("Hello".length, 5);
+  it('current height should be 30', function() {
+    assert.equal(sparky.currentHeight, 'This doggio is 10in tall');
+  });
+
+  it('says woof', () => {
+    const woofs = sparky.woof()
+    assert.equal(woofs, 'Sparky says WOOF!')
+  });
+
+  it('can update weight', () => {
+    const newWeight = 50;
+    sparky.updateWeight = newWeight;
+    assert.equal(sparky.weight, 50)
   })
 })
